8225c2ce12550d0aea504a3c1fa4a5fc49bb6a25
Serj.Korneev <korneev.s@merlion.ru>
2018-04-20 17:08:53 +0300

    add qt-funbox

---
+++ data/funbox/assets/js/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+window.onload = function () {
+  
+};
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIid1c2Ugc3RyaWN0JztcclxuXHJcbndpbmRvdy5vbmxvYWQgPSBmdW5jdGlvbiAoKSB7XHJcbiAgXHJcbn07Il19
