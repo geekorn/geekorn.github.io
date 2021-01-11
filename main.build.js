814604b241a68b9eb303ce6de1e752ed23f6e7b8
Korneev Sergey <korneev.s@citilink.ru>
2021-01-12 00:23:35 +0300

    добавил лендинг пф

--- main.build.js
+++ main.build.js
@@ -92,9 +92,71 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_main_pcss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 /* harmony import */ var _styles_main_pcss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_styles_main_pcss__WEBPACK_IMPORTED_MODULE_0__);
+function _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 
 
 if (false) {}
+
+var popupButtons = document.querySelectorAll('.js--popup-button');
+var popup = document.querySelector('.popup');
+var popupCloseButton = document.querySelector('.popup__close-button');
+var popupOverlay = document.querySelector('.popup__overlay');
+var questions = document.querySelectorAll('.faq__question');
+
+function openPopup() {
+  popup.classList.add('popup_open'); // document.body.style = 'overflow: hidden';
+}
+
+function closePopup() {
+  popup.classList.remove('popup_open'); // document.body.style = 'overflow: visible';
+}
+
+function openQuestion(e) {
+  var target = e.currentTarget;
+  var parent = target.parentElement;
+  parent.classList.toggle('faq__item_open'); // document.body.style = 'overflow: hidden';
+} // console.log('popupButtons', popupButtons);
+
+
+popupButtons.forEach(function (button) {
+  return button.onclick = openPopup;
+});
+popupCloseButton.onclick = closePopup;
+popupOverlay.onclick = closePopup;
+questions.forEach(function (question) {
+  return question.onclick = openQuestion;
+});
+var anchors = document.querySelectorAll('.menu__link');
+
+var _iterator = _createForOfIteratorHelper(anchors),
+    _step;
+
+try {
+  var _loop = function _loop() {
+    var anchor = _step.value;
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      var blockID = anchor.getAttribute('href').substr(1);
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    _loop();
+  }
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
+}
 
 /***/ }),
 /* 1 */
